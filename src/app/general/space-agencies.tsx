a01d7329c3c39e8eb8747f77ad2792adec1fa2c0
iecloudbird <A00303759@student.tus.ie>
2024-08-15 18:34:53 +0100

    Space Agencies Responsive Update

--- src/app/general/space-agencies.tsx
+++ src/app/general/space-agencies.tsx
@@ -17,10 +17,10 @@ const SpaceAgencies = () => {
           </h1>
           <div className="mr-[3rem]">
             <p className="font-hackathoneCabinetGrotesk text-[18px] leading-[1.8rem] text-left">
-              In 2024, NASA collaborated with even more space agency partners to bring the largest annual global hackathon to communities around the world! These space agency partners, along with NASA’s engineers at the Earth Science Division, came up with innovative problem statements (literally out of this world).
+              In <span className="text-hackathone-font-rocket-red font-[600]">2024</span>, NASA collaborated with even more <span className="text-hackathone-font-rocket-red font-[600]">space agency partners</span> to bring the largest annual global hackathon to communities around the world! These space agency partners, along with NASA’s engineers at the <span className="text-hackathone-font-rocket-red font-[600]">Earth Science Division</span>, came up with <span className="text-hackathone-font-rocket-red font-[600]">innovative problem statements</span> (literally out of this world).
             </p><br />
             <p className="font-hackathoneCabinetGrotesk text-[18px] leading-[1.8rem] text-left pt-[0.5rem]">
-              They provide access to this data and other resources around Earth Observations for solving the challenges.
+              They provide access to this data and other resources around <span className="text-hackathone-font-rocket-red font-[600]">Earth Observations</span> for solving the <span className="text-hackathone-font-rocket-red font-[600]">challenges.</span>
             </p>
           </div>
         </div>
@@ -31,40 +31,38 @@ const SpaceAgencies = () => {
   /** Tablet View */
   const tabletView = () => {
     return (
-      <div className="DesktopScreen:hidden MobileScreen:hidden flex flex-col items-center justify-center gap-[36px] mx-[4.34%]">
-        <div>
-          <h1 className="flex flex-col items-center justify-center text-hackathone-font-rocket-red">
+      <div className="DesktopScreen:hidden MobileScreen:hidden flex flex-col items-center justify-center gap-[36px] mx-[4.06%]">
+        <div className="w-full mt-[3%]">
+          <h1 className="font-[700] text-[42px] text-hackathone-font-rocket-red my-2">
             SPACE AGENCIES
           </h1>
-          <Image src={Rocket} width={1000} alt={"Dev Team Icon"}></Image>
-        </div>
-        <div className="">
-          <p className="font-hackathoneCabinetGrotesk font-[400] text-[26px] leading-[223.34%]">
+          <p className="font-hackathoneCabinetGrotesk font-[600] text-[18px] leading-[30.57px] pr-[12.7%]">
             In{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
+            <span className="text-hackathone-font-rocket-red font-[600]">
               2024
             </span>
             , NASA collaborated with even more{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
+            <span className="text-hackathone-font-rocket-red font-[600]">
               space agency partners
             </span>{" "}
             to bring the largest annual global hackathon to communities around
             the world! These space agency partners , along with NASA’s engineers
             at the{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
+            <span className="text-hackathone-font-rocket-red font-[600]">
               Earth Science Division
             </span>
             , came up with{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
+            <span className="text-hackathone-font-rocket-red font-[600]">
               innovative problem statements
             </span>{" "}
-            (literally out of this world). They provide access to this data and
-            other resources around Earth Observations for solving the{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
-              challenges
-            </span>
-            .
+            (literally out of this world).
+          </p><br />
+          <p className="font-hackathoneCabinetGrotesk text-[18px] font-[600] leading-[1.8rem] text-left pt-[0.5rem]">
+            They provide access to this data and other resources around <span className="text-hackathone-font-rocket-red font-[600]">Earth Observations</span> for solving the <span className="text-hackathone-font-rocket-red font-[600]">challenges.</span>
           </p>
+        </div>
+        <div className="w-full mb-[20%]">
+          <AgenciesSlider />
         </div>
       </div>
     );
@@ -73,40 +71,41 @@ const SpaceAgencies = () => {
   /** Mobile View */
   const mobileView = () => {
     return (
-      <div className="DesktopScreen:hidden TabletScreen:hidden flex flex-col items-center justify-center gap-[36px] mx-[4.34%]">
-        <div>
-          <h1 className="flex flex-col items-center justify-center text-hackathone-font-rocket-red">
+      <div className="DesktopScreen:hidden TabletScreen:hidden flex flex-col items-center justify-center mx-[4.06%]">
+        <div className="w-full mt-[3%]">
+          <h1 className="font-[700] text-[30.99px] text-hackathone-font-rocket-red my-2 pb-[0.5rem]">
             SPACE AGENCIES
           </h1>
-          <Image src={Rocket} width={1000} alt={"Dev Team Icon"}></Image>
         </div>
-        <div className="">
-          <p className="font-hackathoneCabinetGrotesk font-[400] text-[24px] leading-[173.34%]">
-            In{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
-              2024
-            </span>
-            , NASA collaborated with even more{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
-              space agency partners
-            </span>{" "}
-            to bring the largest annual global hackathon to communities around
-            the world! These space agency partners , along with NASA’s engineers
-            at the{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
-              Earth Science Division
-            </span>
-            , came up with{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
-              innovative problem statements
-            </span>{" "}
-            (literally out of this world). They provide access to this data and
-            other resources around Earth Observations for solving the{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
-              challenges
-            </span>
-            .
-          </p>
+        <p className="font-hackathoneCabinetGrotesk text-slate-50 font-[500] text-[16px] leading-[30.57px] pr-[8.7%]">
+          In{" "}
+          <span className="text-hackathone-font-rocket-red font-[500]">
+            2024
+          </span>
+          , NASA collaborated with even more{" "}
+          <span className="text-hackathone-font-rocket-red font-[500]">
+            space agency partners
+          </span>{" "}
+          to bring the largest annual global hackathon to communities around
+          the world! These space agency partners , along with NASA’s engineers
+          at the{" "}
+          <span className="text-hackathone-font-rocket-red font-[500]">
+            Earth Science Division
+          </span>
+          , came up with{" "}
+          <span className="text-hackathone-font-rocket-red font-[500]">
+            innovative problem statements
+          </span>{" "}
+          (literally out of this world).
+        </p><br />
+        <p className="font-hackathoneCabinetGrotesk text-slate-50 font-[500] text-[16px] leading-[30.57px] pr-[8.7%] mb-[2rem]">They provide access to this data and
+          other resources around <span className="text-hackathone-font-rocket-red font-[500]">Earth Observations</span> for solving the{" "}
+          <span className="text-hackathone-font-rocket-red font-[500]">
+            challenges
+          </span>
+        </p>
+        <div className="w-full mb-[20%]">
+          <AgenciesSlider />
         </div>
       </div>
     );
