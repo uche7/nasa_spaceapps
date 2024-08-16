99ec94eee50f4f22aa9dac9564cc8acedd17b14a
iecloudbird <A00303759@student.tus.ie>
2024-08-16 13:42:30 +0100

    Menu Button Gradient design

--- src/app/general/navigation-bar.tsx
+++ src/app/general/navigation-bar.tsx
@@ -108,7 +108,15 @@ export default function NavigationBar() {
             }
             onMouseLeave={() => setIsTooltipVisible(false)}
           >
-            {item.text}
+            <motion.div
+              whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.5 },
+              }}
+              className="focus:outline-none rounded-lg px-4 py-2.5 text-center me-2 font-[600] text-slate-100 font-sefarvestCabinetGrotesk hover:text-hackathone-font-rocket-red hover:font-[700] bg-gradient-wave bg-[length:200%_200%] animate-gradientWave">
+              {item.text}
+            </motion.div>
+
             {index === navigationInfo.length - 1 && isTooltipVisible && (
               <motion.div
                 className="absolute left-[90%] transform -translate-x-[50%] bottom-[-25px] z-50"
