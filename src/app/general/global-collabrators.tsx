fea2136c96338abc6bb72edc895663e5048959f5
YagnikManav <yagnik.c@ahduni.edu.in>
2024-08-21 17:56:55 +0530

    global collabration changed

--- src/app/general/global-collabrators.tsx
+++ src/app/general/global-collabrators.tsx
@@ -1,52 +1,93 @@
 import React from "react";
 import Image from "next/image";
 import CollabratorsIcon from "@/assets/images/general/landing-page/space-man.png";
-
+import { AgenciesSlider } from "./agencies-slider";
+import { CollabratorsSlider } from "./collabrators-slider";
 const GlobalCollabrators = () => {
   /** Desktop View */
   const desktopView = () => {
     return (
-      <div className=" TabletScreen:hidden MobileScreen:hidden flex flex-row items-center justify-center gap-[36px] mx-[8.06%]">
-        <div className="w-[123.45%]">
-          <h1 className="my-4 text-hackathone-font-rocket-red font-[700] text-[42px]">
-            Global Collaborators This Year
-          </h1>
-          <p className="font-hackathoneCabinetGrotesk font-[400] text-[18px] leading-[160%]">
-            <span className="text-hackathone-font-rocket-red font-[500]">
-              This year
-            </span>
-            , the NASA International Space Apps Challenge{" "}
-            <span className="text-hackathone-font-rocket-red  font-[500]">
-              Global Organizing Team
-            </span>
-            collaborates with{" "}
-            <span className="text-hackathone-font-rocket-red font-[500]">
-              organizations
-            </span>{" "}
-            from around the world to provide{" "}
-            <span className="text-hackathone-font-rocket-red  font-[500]">
-              participants
-            </span>{" "}
-            and/or
-            <span className="text-hackathone-font-rocket-red  font-[500]">
-              Local Leads
-            </span>{" "}
-            with tools and resources that enable the creation of{" "}
-            <span className="text-hackathone-font-rocket-red  font-[500]">
-              solutions
-            </span>
-            . These tools and resources can be used for free during the event.
-          </p>
-        </div>
-        <div>
-          <Image
-            className="rounded-xl"
-            src={CollabratorsIcon}
-            width={1000}
-            alt={"Dev Team Icon"}
-          ></Image>
+      // <div className=" TabletScreen:hidden MobileScreen:hidden flex flex-row items-center justify-center gap-[36px] mx-[8.06%]">
+      //   <div className="w-[123.45%]">
+      //     <h1 className="my-4 text-hackathone-font-rocket-red font-[700] text-[42px]">
+      //       Global Collaborators This Year
+      //     </h1>
+      //     <p className="font-hackathoneCabinetGrotesk font-[400] text-[18px] leading-[160%]">
+      //       <span className="text-hackathone-font-rocket-red font-[500]">
+      //         This year
+      //       </span>
+      //       , the NASA International Space Apps Challenge{" "}
+      //       <span className="text-hackathone-font-rocket-red  font-[500]">
+      //         Global Organizing Team
+      //       </span>
+      //       collaborates with{" "}
+      //       <span className="text-hackathone-font-rocket-red font-[500]">
+      //         organizations
+      //       </span>{" "}
+      //       from around the world to provide{" "}
+      //       <span className="text-hackathone-font-rocket-red  font-[500]">
+      //         participants
+      //       </span>{" "}
+      //       and/or
+      //       <span className="text-hackathone-font-rocket-red  font-[500]">
+      //         Local Leads
+      //       </span>{" "}
+      //       with tools and resources that enable the creation of{" "}
+      //       <span className="text-hackathone-font-rocket-red  font-[500]">
+      //         solutions
+      //       </span>
+      //       . These tools and resources can be used for free during the event.
+      //     </p>
+      //   </div>
+      //   <div>
+      //     <Image
+      //       className="rounded-xl"
+      //       src={CollabratorsIcon}
+      //       width={1000}
+      //       alt={"Dev Team Icon"}
+      //     ></Image>
+      //   </div>
+      // </div>
+
+      <div className=" TabletScreen:hidden MobileScreen:hidden flex flex-row gap-[36px] mx-[8.06%]">
+         <div className="lg:w-1/2 w-full mb-[20%] mt-[3%] ml-[2rem]">
+        <h1 className="font-[700] text-[42px] text-hackathone-font-rocket-red my-2 pb-[0.5rem]">
+        Global Collaborators This Year
+        </h1>
+        <div className="mr-[3rem]">
+        <p className="font-hackathoneCabinetGrotesk font-[400] text-[18px] leading-[160%]">
+       <span className="text-hackathone-font-rocket-red font-[500]">
+           This year
+             </span>
+             , the NASA International Space Apps Challenge{" "}
+             <span className="text-hackathone-font-rocket-red  font-[500]">
+               Global Organizing Team
+             </span>
+             collaborates with{" "}
+             <span className="text-hackathone-font-rocket-red font-[500]">
+               organizations
+             </span>{" "}
+             from around the world to provide{" "}
+             <span className="text-hackathone-font-rocket-red  font-[500]">
+               participants
+             </span>{" "}
+             and/or
+             <span className="text-hackathone-font-rocket-red  font-[500]">
+               Local Leads
+             </span>{" "}
+             with tools and resources that enable the creation of{" "}
+             <span className="text-hackathone-font-rocket-red  font-[500]">
+               solutions
+             </span>
+             . These tools and resources can be used for free during the event.
+           </p>
         </div>
       </div>
+      <div className="lg:w-1/2 w-full text-center">
+        <CollabratorsSlider />
+      </div>
+     
+    </div>
     );
   };
 
@@ -73,8 +114,8 @@ const GlobalCollabrators = () => {
             </span>{" "}
             and/or
             <span className="text-hackathone-font-rocket-red  font-[500]">
-              Local Leads
-            </span>{" "}
+              Local Leads {" "}
+            </span>
             with tools and resources that enable the creation of{" "}
             <span className="text-hackathone-font-rocket-red  font-[500]">
               solutions
@@ -86,12 +127,7 @@ const GlobalCollabrators = () => {
           <h1 className="flex flex-col items-center justify-center text-hackathone-font-rocket-red">
             Global Collaborators This Year
           </h1>
-          <Image
-            className="rounded-xl"
-            src={CollabratorsIcon}
-            width={1000}
-            alt={"Dev Team Icon"}
-          ></Image>
+          <CollabratorsSlider />
         </div>
       </div>
     );
@@ -133,12 +169,7 @@ const GlobalCollabrators = () => {
           </p>
         </div>
         <div>
-          <Image
-            className="rounded-xl"
-            src={CollabratorsIcon}
-            width={1000}
-            alt={"Dev Team Icon"}
-          ></Image>
+        <CollabratorsSlider />
         </div>
       </div>
     );
--- src/app/general/landing-page.tsx
+++ src/app/general/landing-page.tsx
@@ -31,7 +31,7 @@ const LandingPage = () => {
         <JoinUsPage />
         <SpaceAgencies />
         <GlobalCollabrators />
-        <CollabratorsSlider />
+        {/* <CollabratorsSlider /> */}
         <Shannon />
         <TeamStories />
         <HackathoneTimeline />
