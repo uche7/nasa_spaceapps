7d68e019db99667b9272cb3c570a51dbc3cb0a88
iecloudbird <A00303759@student.tus.ie>
2024-08-22 14:36:59 +0100

    Fixes FAQ PAGE

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -32,60 +32,62 @@ const Faq = () => {
           </Canvas>
         </div>
 
-        <div className="relative z-10 py-[24px] MobileScreen:py-[12px]">
+        <div className="relative z-10 py-[24px] MobileScreen:py-[12px] flex flex-col">
           <NavigationBar />
-          <div className="m-4">
-            <Typography
-              variant="h4"
-              gutterBottom
-              className="lg:px-[6.8%] md:my-[42px] font-hackathoneCabinetGrotesk font-[600] text-hackathone-font-rocket-red"
-            >
-              Frequently Asked Questions (FAQS)
-            </Typography>
-            <section className="flex flex-row item-center justify-between">
-              <div>
-                {faqData.map((item, index) => (
-                  <Accordion
-                    key={index}
-                    expanded={expanded === index}
-                    onChange={handleChange(index)}
-                    className="my-8 border-2 border-hackathone-font-light-grey lg:mx-[9.8%] bg-transparent text-white rounded"
-                    disableGutters
-                  >
-                    <AccordionSummary
-                      expandIcon={
-                        <ExpandMoreIcon
-                          style={{
-                            color: "yellow",
-                          }}
-                        />
-                      }
-                      aria-controls={`panel${index}-content`}
-                      id={`panel${index}-header`}
+          <div className="w-full flex flex-row">
+            <div className="w-[100%]">
+              <Typography
+                variant="h4"
+                gutterBottom
+                className="lg:px-[3.8%] md:my-[2rem] mx-16 font-hackathoneCabinetGrotesk text-hackathone-font-rocket-red font-[800] text-[42px] leading-[44.62px]"
+              >
+                Frequently Asked Questions (FAQS)
+              </Typography>
+              <div className="flex flex-row">
+                <div className="w-[80%] pl-[2.8%]">
+                  {faqData.map((item, index) => (
+                    <Accordion
+                      key={index}
+                      expanded={expanded === index}
+                      onChange={handleChange(index)}
+                      className="my-4 border-2 border-hackathone-font-light-grey lg:mx-[9.8%] bg-transparent text-white rounded"
+                      disableGutters
                     >
-                      <h1
-                        className="font-hackathoneSFProDisplay font-[300] text-[18px]"
-                        style={{
-                          color: expanded === index ? "yellow" : "white",
-                        }}
+                      <AccordionSummary
+                        expandIcon={
+                          <ExpandMoreIcon
+                            style={{
+                              color: "yellow",
+                            }}
+                          />
+                        }
+                        aria-controls={`panel${index}-content`}
+                        id={`panel${index}-header`}
                       >
-                        {item.header}
-                      </h1>
-                    </AccordionSummary>
-                    <AccordionDetails className="">
-                      <Typography>{item.text}</Typography>
-                    </AccordionDetails>
-                  </Accordion>
-                ))}
+                        <h1
+                          className="font-hackathoneSFProDisplay font-[300] text-[18px]"
+                          style={{
+                            color: expanded === index ? "yellow" : "white",
+                          }}
+                        >
+                          {item.header}
+                        </h1>
+                      </AccordionSummary>
+                      <AccordionDetails className="">
+                        <Typography>{item.text}</Typography>
+                      </AccordionDetails>
+                    </Accordion>
+                  ))}
+                </div>
+                <div className="w-[60%]">
+                  <Image
+                    src={Sun}
+                    alt={"Sun Image"}
+                    className="translate-x-[30%] translate-y-[3%]"
+                  ></Image>
+                </div>
               </div>
-              <div className="w-[120%] -mt-[100px]">
-                <Image
-                  src={Sun}
-                  alt={"Sun Image"}
-                  className="translate-x-[20%] translate-y-[3%]"
-                ></Image>
-              </div>
-            </section>
+            </div>
           </div>
 
           <Footer />
