cad76459c3f966fac1372c2ade0a8876138beff9
iecloudbird <A00303759@student.tus.ie>
2024-09-16 15:51:18 +0100

    members detail structure

---
+++ src/app/teams/teams.dto.ts
@@ -0,0 +1,114 @@
+// import Fwf from "@/assets/images/general/landing-page/winners/Soundsurfers.png";
+
+export const teamMembers = [
+    {
+        name: 'Sahir Sharma',
+        role: 'Lead',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Manav Shah',
+        role: 'Co-Lead',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Nitish Malik',
+        role: 'Co-Lead',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Shean Hans',
+        role: 'Lead Wed Developer',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Manav Yagnik',
+        role: 'Web Developer',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Mavis',
+        role: 'Web Developer',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Ucchena Nangifor',
+        role: 'Web Developer',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Sudhanshu Bhamburkar',
+        role: 'QA & Web developer',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Ishita Malik',
+        role: 'Content writer',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+    {
+        name: 'Shaiv Mehrotra, ',
+        role: 'Content writer',
+        details: [
+            [
+                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "HackAthlone", highlight: true },
+            ],
+        ],
+        // image: Fwf,
+    },
+]
