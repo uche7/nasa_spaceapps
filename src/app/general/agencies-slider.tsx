512a743b1295fa701582c27f86bdffa8a4173d50
iecloudbird <kuroxxxshiro9@gmail.com>
2024-09-27 01:33:58 +0100

    enlarge icon

--- src/app/general/agencies-slider.tsx
+++ src/app/general/agencies-slider.tsx
@@ -29,13 +29,13 @@ export const AgenciesSlider = () => {
           return (
             <div
               key={index}
-              className={`flex items-center justify-center px-4 py-2 max-h-[100px] ${index === sliderItems.length - 1 ? "col-span-2 md:col-span-1 lg:col-span-2 sm:scale-150" : ""
+              className={`flex items-center justify-center px-4 py-2 max-h-[100px] ${index === sliderItems.length - 1 ? "col-span-2 md:col-span-1 lg:col-span-2 sm:scale-125" : ""
                 }`}
               style={{ transition: "transform 0.3s ease-in-out" }} // Add smooth transition
 
             >
               <Image
-                className="cursor-pointer transform transition-transform duration-300 lg:hover:scale-125 hover:scale-110 sm:w-[80px] lg:min-h-[20px]"
+                className={`cursor-pointer transform transition-transform duration-300 lg:hover:scale-125 hover:scale-110 sm:w-[80px] ${index === sliderItems.length - 1 ? "lg:min-w-[130px] MobileScreen:min-w-[130px]" : ""}`}
                 src={imgSrcs[index]}
                 width={120}
                 height={120}
