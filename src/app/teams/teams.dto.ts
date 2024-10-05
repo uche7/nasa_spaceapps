f9c85efa00696f7805ee6ba7ffee345c662d4b71
iecloudbird <kuroxxxshiro9@gmail.com>
2024-10-05 13:25:09 +0100

    update image sizing

--- src/app/teams/teams.dto.ts
+++ src/app/teams/teams.dto.ts
@@ -1,17 +1,17 @@
 import Fwf from "@/assets/images/general/landing-page/winners/Soundsurfers.png";
 import Arjun from "@/assets/images/general/landing-page/teams/Arjun S Narayanan.png";
 import Kshitij from "@/assets/images/general/landing-page/teams/Kshitij Motke.jpg";
-import Sahir from "@/assets/images/general/landing-page/teams/Sahir Sharma.jpeg";
-import Sudhanshu from "@/assets/images/general/landing-page/teams/Sudhanshu Bhamburkar.jpeg";
-import Mavis from "@/assets/images/general/landing-page/teams/Mavis Hye Xuan Chia.jpeg";
+import Sahir from "@/assets/images/general/landing-page/teams/Sahir Sharma.jpg";
+import Sudhanshu from "@/assets/images/general/landing-page/teams/Sudhanshu Bhamburkar.jpg";
+import Mavis from "@/assets/images/general/landing-page/teams/Mavis Hye Xuan Chia.jpg";
 import Shean from "@/assets/images/general/landing-page/teams/Shean Hans Teoh.jpg";
 import Nora from "@/assets/images/general/landing-page/teams/Nguyen Phung (Nora).jpg";
 import Nnajiofor from "@/assets/images/general/landing-page/teams/Nnajiofor Uchenna Franklin.jpg";
 import Khushi from "@/assets/images/general/landing-page/teams/Khushi Mehta.png";
 import Catherine from "@/assets/images/general/landing-page/teams/Catherine Pulickan.jpg";
 import YuJing from "@/assets/images/general/landing-page/teams/Yujing Zhang.png";
-import Tvisha from "@/assets/images/general/landing-page/teams/Tvisha Mehta.jpeg";
-import Khayati from "@/assets/images/general/landing-page/teams/Khayati Arora.jpeg";
+import Tvisha from "@/assets/images/general/landing-page/teams/Tvisha Mehta.jpg";
+import Khayati from "@/assets/images/general/landing-page/teams/Khayati Arora.jpg";
 import Manav from "@/assets/images/general/landing-page/teams/Manav.png";
 import Nitish from "@/assets/images/general/landing-page/teams/Nitish.jpg";
 import Yagnik from "@/assets/images/general/landing-page/teams/Yagnik.jpg";
@@ -21,25 +21,25 @@ export const teamMembers = [
     {
         name: 'Sahir Sharma',
         role: 'Lead',
-        details: null,
+        bio: null,
         image: Sahir,
     },
     {
         name: 'Manav Shah',
         role: 'Co-Lead',
-        details: null,
+        bio: null,
         image: Manav,
     },
     {
         name: 'Nitish Malik',
         role: 'Co-Lead',
-        details: null,
+        bio: null,
         image: Nitish,
     },
     {
         name: 'Shean Hans',
         role: 'Lead Wed Developer',
-        details: [
+        bio: [
             [
                 { text: "Always learning, glad to be a part of ", highlight: false },
                 { text: "HackAthlone", highlight: true },
@@ -50,67 +50,67 @@ export const teamMembers = [
     {
         name: 'Manav Yagnik',
         role: 'Web Developer',
-        details: null,
+        bio: null,
         image: Yagnik,
     },
     {
         name: 'Arjun S Narayanan',
         role: 'Designer',
-        details: null,
+        bio: null,
         image: Arjun,
     },
     {
         name: 'Mavis',
         role: 'Web Developer',
-        details: null,
+        bio: null,
         image: Mavis,
     },
     {
         name: 'Ucchena Nangifor',
         role: 'Web Developer',
-        details: null,
+        bio: null,
         image: Nnajiofor,
     },
     {
         name: 'Sudhanshu Bhamburkar',
         role: 'QA & Web developer',
-        details: null,
+        bio: null,
         image: Sudhanshu,
     },
     {
         name: 'Khushi Mehta',
-        role: 'Volunteer',
-        details: null,
+        role: 'Designer ',
+        bio: null,
         image: Khushi,
     },
     {
         name: 'Catherine Pulickan',
         role: 'Designer',
-        details: null,
+        bio: null,
         image: Catherine,
     },
     {
         name: 'Tvisha Mehta',
         role: 'Designer',
-        details: null,
+        bio: null,
         image: Tvisha,
     },
     {
         name: 'Khayati Arora',
         role: 'Designer',
-        details: null,
+        bio: null,
         image: Khayati,
     },
     {
         name: 'Yujing Zhang',
         role: 'Ground Support',
-        details: null,
+        bio: null,
         image: YuJing,
     },
     {
         name: 'Nguyen Phung (Nora)',
         role: 'Video Editor',
-        details: null,
+        bio: null,
         image: Nora,
     },
 ]
