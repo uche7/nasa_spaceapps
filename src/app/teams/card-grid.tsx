8004eb7c9f900fd36325f093975abf3b9206133f
iecloudbird <kuroxxxshiro9@gmail.com>
2024-10-05 19:20:49 +0100

    finish touch up

--- src/app/teams/card-grid.tsx
+++ src/app/teams/card-grid.tsx
@@ -29,7 +29,7 @@ export const CardGrid = () => {
 
     const renderSocialIcons = (socials: any) => {
         return (
-            <div className="flex justify-center space-x-4 mt-4">
+            <div className="flex justify-start lg:justify-center space-x-4 mt-4 ml-6">
                 {socials.twitter && (
                     <a href={socials.twitter} target="_blank" rel="noopener noreferrer">
                         <Image
@@ -44,7 +44,7 @@ export const CardGrid = () => {
                 {socials.linkedin && (
                     <a href={socials.linkedin} target="_blank" rel="noopener noreferrer">
                         <Image
-                            className="cursor-pointer w-6 h-6 ms-2 MobileScreen:w-4 MobileScreen:h-4 hover:scale-110"
+                            className="cursor-pointer w-6 h-6 hover:scale-110"
                             src={InIcon}
                             height={80}
                             width={80}
@@ -55,6 +55,17 @@ export const CardGrid = () => {
                 {socials.github && (
                     <a href={socials.github} target="_blank" rel="noopener noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-6 h-6 text-white hover:text-gray-400 hover:scale-110" viewBox="0 0 24 24"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.302 3.438 9.8 8.205 11.387.6.113.82-.258.82-.577v-2.234c-3.338.724-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.757-1.333-1.757-1.09-.744.084-.729.084-.729 1.205.084 1.838 1.234 1.838 1.234 1.07 1.835 2.809 1.304 3.495.997.108-.775.418-1.304.76-1.604-2.665-.304-5.466-1.334-5.466-5.93 0-1.31.467-2.38 1.235-3.221-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.301 1.23.96-.267 1.983-.399 3.003-.404 1.02.005 2.043.137 3.004.404 2.29-1.552 3.297-1.23 3.297-1.23.653 1.653.241 2.873.118 3.176.77.841 1.234 1.911 1.234 3.221 0 4.609-2.804 5.623-5.476 5.921.43.371.823 1.103.823 2.222v3.293c0 .321.218.694.824.577C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
+                    </a>
+                )}
+                {socials.instagram && (
+                    <a href={socials.instagram} target="_blank" rel="noopener noreferrer">
+                        <Image
+                            className="cursor-pointer w-6 h-6 ms-2 MobileScreen:w-4 MobileScreen:h-4 hover:scale-110"
+                            src={InstaIcon}
+                            height={80}
+                            width={80}
+                            alt={"InstaIcon"}
+                        />
                     </a>
                 )}
             </div>
@@ -70,7 +81,7 @@ export const CardGrid = () => {
                         <div
                             key={index}
                             className={`flex flex-col items-start justify-center text-center start-[4rem]`}
-                            style={{ transition: "transform 0.3s ease-in-out" }} // Add smooth transition
+                            style={{ transition: "transform 0.3s ease-in-out" }} // Smooth transition
                             onClick={() => handleCardClick(index)}
                         >
                             {/* Container for the image */}
@@ -85,23 +96,23 @@ export const CardGrid = () => {
                                 />
                             </div>
                             {/* Container for the text */}
-                            <div className="w-80 text-start text-lg text-slate-50 py-4 pl-2 MobileScreen:pl-4">
-                                <p>{item.name}</p>
-                                <p>{item.role}</p>
+                            <div className="w-80 text-start text-lg text-slate-50 py-4 pl-2 MobileScreen:pl-4 text-hackathoneCabinetGrotesk">
+                                <p className="font-bold">{item.name}</p>
+                                <p className="text-red-400">{item.role}</p>
                             </div>
                         </div>
                     );
                 })}
             </div>
             {/* Popup Modal */}
-            {selectedMember !== null && gridItems[selectedMember].bio && (
+            {selectedMember !== null && (gridItems[selectedMember].socials.instagram || gridItems[selectedMember].socials.linkedin) && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
                     onClick={handleOutsideClick}>
                     <motion.div
                         initial={{ opacity: 0, y: -50 }}
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -50 }}
-                        className="bg-gray-800 p-6 rounded-2xl shadow-lg w-11/12 sm:w-9/12 md:w-8/12 lg:w-6/12 xl:w-5/12 text-white relative"
+                        className="bg-gray-800 p-6 rounded-2xl shadow-lg w-11/12 sm:w-7/12 md:w-7/12 lg:w-6/12 xl:w-5/12 text-white relative"
                         ref={modalRef}
                     >
                         <button
@@ -123,51 +134,37 @@ export const CardGrid = () => {
                             </div>
                         </div>
 
+                        <div className="ml-6">
+                            <h2 className="text-2xl md:text-3xl font-bold mb-2">
+                                {gridItems[selectedMember].name}
+                            </h2>
+                            <p className="text-md md:text-lg mb-4 text-red-500 font-bold">
+                                {gridItems[selectedMember].role}
+                            </p>
 
-                        <h2 className="text-3xl font-bold mb-2">
-                            {gridItems[selectedMember].name}
-                        </h2>
-                        <p className="text-lg mb-4">
-                            {gridItems[selectedMember].role}
-                        </p>
+                            {/* Display bio or other details */}
+                            <p className="text-md mb-6">
+                                {gridItems[selectedMember].bio ? (
+                                    gridItems[selectedMember].bio.map((bioArray, index) => (
+                                        <span key={index}>
+                                            {bioArray.map((bio, subIndex) => (
+                                                <span
+                                                    key={subIndex}
+                                                    className={bio.highlight ? "font-bold text-red-500" : ""}
+                                                >
+                                                    {bio.text}
+                                                </span>
+                                            ))}
+                                        </span>
+                                    ))
+                                ) : (
+                                    "Check for socials media below:"
+                                )}
+                            </p>
+                        </div>
 
-                        {/* Display bio or other details */}
-                        <p className="text-md mb-4">
-
-                            {gridItems[selectedMember].bio ? (
-                                gridItems[selectedMember].bio.map((bioArray, index) => (
-                                    <span key={index}>
-                                        {bioArray.map((bio, subIndex) => (
-                                            <span
-                                                key={subIndex}
-                                                className={bio.highlight ? "font-bold text-red-500" : ""}
-                                            >
-                                                {bio.text}
-                                            </span>
-                                        ))}
-                                    </span>
-                                ))
-                            ) : (
-                                "Check for socials media below:."
-                            )}
-                        </p>
-
-                        { }
 
                         {/* Social Media Links */}
-                        {/* <div className="flex justify-center space-x-4 mt-4">
-
-                            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                                <Image
-                                    className="cursor-pointer w-6 h-6 ms-2 MobileScreen:w-4 MobileScreen:h-4 hover:scale-110"
-                                    src={InIcon}
-                                    height={80}
-                                    width={80}
-                                    alt={"InIcon"}
-                                />
-                            </a>
-
-                        </div> */}
                         {renderSocialIcons(gridItems[selectedMember].socials)}
                     </motion.div>
                 </div>
