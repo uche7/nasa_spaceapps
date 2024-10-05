e7f3ee5a18bd9b2d69f6aba0b008ee3b99e1e148
iecloudbird <kuroxxxshiro9@gmail.com>
2024-10-05 12:01:24 +0100

    complete team images upload

--- src/app/teams/teams.dto.ts
+++ src/app/teams/teams.dto.ts
@@ -1,114 +1,116 @@
 import Fwf from "@/assets/images/general/landing-page/winners/Soundsurfers.png";
+import Arjun from "@/assets/images/general/landing-page/teams/Arjun S Narayanan.png";
+import Kshitij from "@/assets/images/general/landing-page/teams/Kshitij Motke.jpg";
+import Sahir from "@/assets/images/general/landing-page/teams/Sahir Sharma.jpeg";
+import Sudhanshu from "@/assets/images/general/landing-page/teams/Sudhanshu Bhamburkar.jpeg";
+import Mavis from "@/assets/images/general/landing-page/teams/Mavis Hye Xuan Chia.jpeg";
+import Shean from "@/assets/images/general/landing-page/teams/Shean Hans Teoh.jpg";
+import Nora from "@/assets/images/general/landing-page/teams/Nguyen Phung (Nora).jpg";
+import Nnajiofor from "@/assets/images/general/landing-page/teams/Nnajiofor Uchenna Franklin.jpg";
+import Khushi from "@/assets/images/general/landing-page/teams/Khushi Mehta.png";
+import Catherine from "@/assets/images/general/landing-page/teams/Catherine Pulickan.jpg";
+import YuJing from "@/assets/images/general/landing-page/teams/Yujing Zhang.png";
+import Tvisha from "@/assets/images/general/landing-page/teams/Tvisha Mehta.jpeg";
+import Khayati from "@/assets/images/general/landing-page/teams/Khayati Arora.jpeg";
+import Manav from "@/assets/images/general/landing-page/teams/Manav.png";
+import Nitish from "@/assets/images/general/landing-page/teams/Nitish.jpg";
+import Yagnik from "@/assets/images/general/landing-page/teams/Yagnik.jpg";
+
 
 export const teamMembers = [
     {
         name: 'Sahir Sharma',
         role: 'Lead',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        details: null,
+        image: Sahir,
     },
     {
         name: 'Manav Shah',
         role: 'Co-Lead',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        details: null,
+        image: Manav,
     },
     {
         name: 'Nitish Malik',
         role: 'Co-Lead',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        details: null,
+        image: Nitish,
     },
     {
         name: 'Shean Hans',
         role: 'Lead Wed Developer',
         details: [
             [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
+                { text: "Always learning, glad to be a part of ", highlight: false },
                 { text: "HackAthlone", highlight: true },
             ],
         ],
-        image: { Fwf },
+        image: Shean,
     },
     {
         name: 'Manav Yagnik',
         role: 'Web Developer',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        details: null,
+        image: Yagnik,
+    },
+    {
+        name: 'Arjun S Narayanan',
+        role: 'Designer',
+        details: null,
+        image: Arjun,
     },
     {
         name: 'Mavis',
         role: 'Web Developer',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        details: null,
+        image: Mavis,
     },
     {
         name: 'Ucchena Nangifor',
         role: 'Web Developer',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        details: null,
+        image: Nnajiofor,
     },
     {
         name: 'Sudhanshu Bhamburkar',
         role: 'QA & Web developer',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        details: null,
+        image: Sudhanshu,
     },
     {
-        name: 'Ishita Malik',
-        role: 'Content writer',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        name: 'Khushi Mehta',
+        role: 'Volunteer',
+        details: null,
+        image: Khushi,
     },
     {
-        name: 'Shaiv Mehrotra, ',
-        role: 'Content writer',
-        details: [
-            [
-                { text: "SpaceApps Athlone, also known as ", highlight: false },
-                { text: "HackAthlone", highlight: true },
-            ],
-        ],
-        image: { Fwf },
+        name: 'Catherine Pulickan',
+        role: 'Designer',
+        details: null,
+        image: Catherine,
+    },
+    {
+        name: 'Tvisha Mehta',
+        role: 'Designer',
+        details: null,
+        image: Tvisha,
+    },
+    {
+        name: 'Khayati Arora',
+        role: 'Designer',
+        details: null,
+        image: Khayati,
+    },
+    {
+        name: 'Yujing Zhang',
+        role: 'Ground Support',
+        details: null,
+        image: YuJing,
+    },
+    {
+        name: 'Nguyen Phung (Nora)',
+        role: 'Video Editor',
+        details: null,
+        image: Nora,
     },
 ]
